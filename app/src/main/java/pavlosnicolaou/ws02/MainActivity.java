7f74f814ee09a99d2f3bbd819a641bcb45f4ad54
p4vlos <nicolaou_paul@hotmail.com>
2017-02-16 17:14:32 +0000

    finished with this project

--- app/src/main/java/pavlosnicolaou/ws02/MainActivity.java
+++ app/src/main/java/pavlosnicolaou/ws02/MainActivity.java
@@ -1,13 +1,28 @@
 package pavlosnicolaou.ws02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImageThunbnail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageThunbnail = (ImageView) findViewById(R.id.mainImage);
+        ImageThunbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
